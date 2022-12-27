763c3cc763c5c2235a135f420da6678c5a49e916
rohitkalyanpuri <112562388+CamRohitkp@users.noreply.github.com>
2022-12-27 10:48:28 +0530

    Update ExportExcelController.cs

--- Pupil.WebApi/Controllers/ExportExcelController.cs
+++ Pupil.WebApi/Controllers/ExportExcelController.cs
@@ -18,15 +18,17 @@ namespace Pupil.WebApi.Controllers
         private readonly IParentService _parentService;
         private readonly IGradeService _gradeService;
         private readonly IDivisionService _divisionService;
+        private readonly IAcademicYearService _academicYearService;
 
-        public ExportExcelController(IParentService parentService,IGradeService gradeService,IDivisionService divisionService)
+        public ExportExcelController(IParentService parentService,IGradeService gradeService, IDivisionService divisionService, IAcademicYearService academicYearService)
         {
             _parentService = parentService;
             _gradeService = gradeService;
             _divisionService = divisionService;
+            _academicYearService = academicYearService;
         }
 
-        [HttpGet]
+        [HttpGet,Route("student")]
         public async Task<IActionResult> WriteDataToExcel()
         {
             DataTable dt = GetStudentColumnHeader();
@@ -40,30 +42,42 @@ namespace Pupil.WebApi.Controllers
                 var Gradeoptions = await _gradeService.GetGradesForExcel();
                 var Parentoptions = await _parentService.GetParentsForExcel();
                 var Divisionoptions = await _divisionService.GetDivisionsForExcel();
+                var Academicoptions = await _academicYearService.GetAcademicYearForExcel();
                 var validGradeOptions = $"\"{String.Join(",", Gradeoptions)}\"";
                 var validParentOptions = $"\"{String.Join(",", Parentoptions)}\"";
                 var validDivisionOptions = $"\"{String.Join(",", Divisionoptions)}\"";
-                //Grades
-                worksheet.Range("D2:D500").SetDataValidation().IgnoreBlanks = true;
-                worksheet.Range("D2:D500").SetDataValidation().InCellDropdown = true;
-                worksheet.Range("D2:D500").Value = "---";
-                worksheet.Range("D2:D500").SetDataValidation().List(validGradeOptions, true);
+                var validAcademicOptions = $"\"{String.Join(",", Academicoptions)}\"";
 
-
-                //Division
+                //Academics
                 worksheet.Range("E2:E500").SetDataValidation().IgnoreBlanks = true;
                 worksheet.Range("E2:E500").SetDataValidation().InCellDropdown = true;
                 worksheet.Range("E2:E500").Value = "---";
-                worksheet.Range("E2:E500").SetDataValidation().List(validDivisionOptions, true);
+                worksheet.Range("E2:E500").SetDataValidation().List(validAcademicOptions, true);
 
 
-                //Parent
+                //Grades
                 worksheet.Range("F2:F500").SetDataValidation().IgnoreBlanks = true;
                 worksheet.Range("F2:F500").SetDataValidation().InCellDropdown = true;
                 worksheet.Range("F2:F500").Value = "---";
-                worksheet.Range("F2:F500").SetDataValidation().List(validParentOptions, true);
+                worksheet.Range("F2:F500").SetDataValidation().List(validGradeOptions, true);
 
-                
+
+                //Division
+                worksheet.Range("G2:G500").SetDataValidation().IgnoreBlanks = true;
+                worksheet.Range("G2:G500").SetDataValidation().InCellDropdown = true;
+                worksheet.Range("G2:G500").Value = "---";
+                worksheet.Range("G2:G500").SetDataValidation().List(validDivisionOptions, true);
+
+
+                //Parent
+                worksheet.Range("H2:H500").SetDataValidation().IgnoreBlanks = true;
+                worksheet.Range("H2:H500").SetDataValidation().InCellDropdown = true;
+                worksheet.Range("H2:H500").Value = "---";
+                worksheet.Range("H2:H500").SetDataValidation().List(validParentOptions, true);
+
+               
+
+
                 using (MemoryStream stream = new MemoryStream())
                 {
                     wb.SaveAs(stream);
@@ -84,10 +98,13 @@ namespace Pupil.WebApi.Controllers
             dt.Columns.Add("Name", typeof(string));
             dt.Columns.Add("Phone Number", typeof(string));
             dt.Columns.Add("Enrollment ID", typeof(string));
+            dt.Columns.Add("Enrollment Date", typeof(string));
+            dt.Columns.Add("AcademicYear-AcademicID", typeof(string));
             dt.Columns.Add("Grade-GradeID", typeof(string));
             dt.Columns.Add("Division-DivisionID", typeof(string));
             dt.Columns.Add("Parent-ParentID", typeof(string));
             dt.Columns.Add("Parent Parent Number", typeof(string));
+            
             //Add Rows in DataTable  
             //dt.Rows.Add(1, "Anoop Kumar Sharma", "Delhi");
             //dt.Rows.Add(2, "Andrew", "U.P.");
